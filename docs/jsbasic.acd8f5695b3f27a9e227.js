820d5bf14ae2fc8115a94f076cb246b99fc73d63
Anna Komkova <ann.komkova98@gmail.com>
2024-01-26 12:31:13 +0300

    build

---
+++ docs/jsbasic.acd8f5695b3f27a9e227.js
@@ -0,0 +1,70 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+
+// function hello() {
+//   const header = document.querySelector('h1')
+//   if (header.innerHTML === 'Hello!') {
+//     header.innerHTML = 'Bye!'
+//   } else {
+//     header.innerHTML = 'Hello!'
+//   }
+// }
+
+// let counter = 0
+// function count() {
+//   counter++
+//   document.querySelector('h1').innerHTML = counter
+
+//   if (counter % 10 === 0) {
+//     alert(`Count is now ${counter}`)
+//   }
+// }
+
+// let counter = 0
+// function count() {
+//   counter++
+//   document.querySelector('h1').innerHTML = counter
+
+//   if (counter % 10 === 0) {
+//     alert(`Count is now ${counter}`)
+//   }
+// }
+// //v1
+// document.addEventListener('DOMContentLoaded', function () {
+//   document.querySelector('button').onclick = count
+// })
+// //v2
+// document.addEventListener('DOMContentLoaded', function () {
+//   document.querySelector('button').addEventListener('click', count)
+// })
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   document.querySelector('form').onsubmit = function () {
+//     const name = document.querySelector('#name').value
+//     alert(`Привет, ${name}, рады тебя видеть!`)
+//   }
+// })
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   document.querySelector('.red').onclick = function () {
+//     document.querySelector('.red').classList.remove('border')
+//   }
+//   document.querySelector('.gold').onclick = function () {
+//     document.querySelector('.gold').classList.add('hidden')
+//   }
+//   document.querySelector('.blue').onclick = function () {
+//     document.querySelector('.blue').classList.toggle('green')
+//   }
+// })
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('button').forEach(function (button) {
+    button.onclick = function () {
+      document.querySelector('h1').style.color = button.dataset.color;
+    };
+  });
+});
+/******/ })()
+;
